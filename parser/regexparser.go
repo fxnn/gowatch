3876d230e9fcd2bd74d15b265a5c80c2727d73a6
Felix <dev@fxnn.de>
2015-03-28 00:16:50 +0100

    RegexpParser: code improvements

--- parser/regexparser.go
+++ parser/regexparser.go
@@ -22,29 +22,54 @@ func (p *RegexpParser) Parse() <-chan logentry.LogEntry {
 }
 
 func (p *RegexpParser) lineToLogEntry(line string, entry *logentry.LogEntry) {
+	matches := p.regexp.FindStringSubmatch(line)
+	for matchNumber, matchedContent := range matches {
+		if matchName, ok := p.submatchNameMap[matchNumber]; ok {
+			p.matchToLogEntry(matchName, matchedContent, entry)
+		}
+	}
+}
+
+func (p *RegexpParser) matchToLogEntry(matchName string, matchedContent string, entry *logentry.LogEntry) {
 	pointerToEntryValue := reflect.ValueOf(entry)
 	entryValue := pointerToEntryValue.Elem()
 
-	matches := p.regexp.FindStringSubmatch(line)
-	for matchNumber, match := range matches {
-		if name, ok := p.submatchNameMap[matchNumber]; ok {
-			field := entryValue.FieldByName(name)
-			if field.IsValid() && field.CanSet() {
-				if field.Kind() == reflect.String {
-					field.SetString(match)
-				} else if field.Kind() == reflect.Int && name == "Level" {
-					matchAsLogLevel, err := logentry.LevelFromString(match)
-					if err != nil {
-						log.Print(err)
-					} else {
-						field.SetInt(int64(matchAsLogLevel))
-					}
-				} else if field.Kind() == reflect.Slice { // esp. for Tags
-					field.Set(reflect.Append(field, reflect.ValueOf(match)))
-				}
-			} else {
-				entry.Custom[name] = match
-			}
+	field := entryValue.FieldByName(matchName)
+	switch {
+	case isAnyStringField(field):
+		field.SetString(matchedContent)
+	case isLogLevelField(matchName, field):
+		matchAsLogLevel, err := logentry.LevelFromString(matchedContent)
+		if err == nil {
+			field.SetInt(int64(matchAsLogLevel))
+		} else {
+			log.Print("Invalid log level [" + matchedContent + "]: ", err)
 		}
+	case isTagsField(matchName, field):
+		field.Set(reflect.Append(field, reflect.ValueOf(matchedContent)))
+	case isCustomField(field):
+		entry.Custom[matchName] = matchedContent
+	default:
+		log.Print("Field not supported as Regexp target: ", matchName)
 	}
+}
+
+func isLogLevelField(fieldName string, field reflect.Value) bool {
+	return isAnyField(field) && field.Kind() == reflect.Int && fieldName == "Level"
+}
+
+func isTagsField(fieldName string, field reflect.Value) bool {
+	return isAnyField(field) && field.Kind() == reflect.Slice && fieldName == "Tags"
+}
+
+func isAnyStringField(field reflect.Value) bool {
+	return isAnyField(field) && field.Kind() == reflect.String
+}
+
+func isCustomField(field reflect.Value) bool {
+	return !isAnyField(field)
+}
+
+func isAnyField(field reflect.Value) bool {
+	return field.IsValid() && field.CanSet()
 }
