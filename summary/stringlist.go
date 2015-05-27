1b0305a7a1d7c0b55586b3284b3b45c84c45336e
Felix Neumann <dev@fxnn.de>
2015-05-27 21:28:53 +0200

    documentation

--- summary/stringlist.go
+++ summary/stringlist.go
@@ -4,7 +4,8 @@ import (
 	"strings"
 )
 
-// taken from golang.org/x/text/collate/sort_test.go, implements collate's Lister interface
+// A list of strings that might be swapped, accessed by index, and joined alltogether.
+// Taken from golang.org/x/text/collate/sort_test.go, implements collate's Lister interface
 type stringList []string
 
 func (s stringList) Append(str string) stringList {
@@ -23,10 +24,12 @@ func (s stringList) Bytes(i int) []byte {
 	return []byte(s[i])
 }
 
+// Joins all the strings in this stringList, separating each by the given separator.
 func (s stringList) Join(sep string) string {
 	return strings.Join(s, sep)
 }
 
+// Returns the result of Join(sep) as []byte.
 func (s stringList) JoinBytes(sep string) []byte {
 	return []byte(s.Join(sep))
 }
